c34adbe02dfcb7b314a513e3df4252869f62762c
Fabio Rafael Dias Viana <102665846+Fabio2500@users.noreply.github.com>
2023-10-16 08:29:19 -0300

    Update Fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -7,7 +7,7 @@ public static int fatorial( int numero){
 	int f=1,r;
 	int i=0;
 	for(i=2;i<=numero;i++){
-		f=f*i;
+	f=f*i;
 	}
 	
 	return f;
@@ -16,13 +16,13 @@ public static int fatorial( int numero){
 
 
 
-	public static void main(String[]args){
-		Scanner sc = new Scanner(System.in);
-		int num,fat,i;
-		i=0;
-        System.out.println("digite o numero para tirar o fatorial:");
-        num = sc.nextInt();
-        System.out.println("fatorial de "+ num + ": " + fatorial(num));
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+int num,fat,i;
+i=0;
+System.out.println("digite o numero para tirar o fatorial:");
+num = sc.nextInt();
+System.out.println("fatorial de "+ num + ": " + fatorial(num));
        
         
        
